bbc8f6ddc839e9372e8fea16fa1477c672951a70
Ozols <its27eo1@odin.local>
2016-04-02 11:18:33 +0300

    Start of task 2

---
+++ Masivu kartosana/Nr2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+void sort2();
+
+int main()
+{
+	string lohs;
+
+	sort2();
+	cout << "Please write yes!" << endl;
+	cin >> lohs;
+	cout << lohs;
+	
+	system("PAUSE");
+	return 0;	
+}
+
+
+void sort2()
+{
+	cout << "LOHS ESI?" << endl;
+
+
+
+}
